a72ea568a24a818313218c5b8f09bc6a0f73575e
Yutian <yutiancoding@gmail.com>
2022-06-02 22:55:00 -0400

    database first approach done

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Marketplace_Web_Portal.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public byte[] UserImage { get; set; }
+        public string UserType { get; set; }
+    }
+}
---
+++ Models/UserDatabaseContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Marketplace_Web_Portal.Models
+{
+    public partial class UserDatabaseContext : DbContext
+    {
+        public UserDatabaseContext()
+        {
+        }
+
+        public UserDatabaseContext(DbContextOptions<UserDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=UserDatabase;Trusted_Connection=False;User=SA;Password=Zyt930615*");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Email, "UQ__Users__AB6E6164F1BF8312")
+                    .IsUnique();
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.UserImage)
+                    .HasColumnType("image")
+                    .HasColumnName("user_image");
+
+                entity.Property(e => e.UserType)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("user_type");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
